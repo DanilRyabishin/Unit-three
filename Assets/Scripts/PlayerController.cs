1f4cb03b0909198f457a262c3d5bad69f633aa35
Danil Ryabishin <d.ryabishin@carx-tech.com>
2025-03-18 14:32:20 +0300

    change props

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace glof
+namespace golf
 { 
-public class NewBehaviourScript : MonoBehaviour
-{
-   
-        
+    public class PlayerController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                Debug.Log("X key down");
+            }
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Debug.Log("Z key down");
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Space key down");
+            }
+        }
+
     }
 }
