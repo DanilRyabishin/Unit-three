07894ccb3441ce81cba91527439e9950805167d7
Danil Ryabishin <d.ryabishin@carx-tech.com>
2025-03-27 19:06:11 +0300

    add minmax delay

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -8,13 +8,17 @@ namespace Golf
     public class LevelController : MonoBehaviour
     {
         public Spawner spawner;
-        private float delay = 2.5f;        
+        public float delayMax = 4f;
+        public float delayMin = 0.3f;
+        public float delayStep = 0.1f;
+
+        private float m_delay = 2.0f;        
         private float m_lastSpawnedTime = 0;
 
         private void Start()
         {
             m_lastSpawnedTime = Time.time;
-            Ball.onCollisionBall += GameOver;
+            RefreshDelay();
         }
         private void OnEnable()
         {           
@@ -27,16 +31,23 @@ namespace Golf
 
         private void GameOver()
         {
-            Debug.Log("Конец игры, пес");
+            Debug.Log("Конец игры!");
             enabled = false;
+        }
+
+        public void RefreshDelay()
+        {
+            m_delay = UnityEngine.Random.Range(delayMin, delayMax);
+            delayMax = Mathf.Max(delayMin, delayMax - delayStep);
         }
         
         private void Update()
         {          
-                if (Time.time >= m_lastSpawnedTime + delay)
+                if (Time.time >= m_lastSpawnedTime + m_delay)
                 {
                     spawner.Spawn();
                     m_lastSpawnedTime = Time.time;
+                RefreshDelay();
                 }
             
         }
