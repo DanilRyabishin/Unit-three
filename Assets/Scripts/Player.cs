f5e0164a5ec9da9f81781147dc1cc6c7776acc95
Danil Ryabishin <d.ryabishin@carx-tech.com>
2025-04-07 10:10:48 +0300

    Change position stick

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,24 +10,24 @@ namespace Golf
         public Transform helper;
         private Vector3 m_lastPos;
         private bool m_isDown = false;
-        public float range = 60f;
+        public float range = 35f;
         public float speed = 1000f;
-        public float power = 15f;
+        public float power = 17f;
 
         private void Update()
         {
             m_lastPos = helper.position;
-           
+            
             Quaternion rot = stick.localRotation;
             Quaternion toRot = Quaternion.Euler(m_isDown ? range : -range, 0, 0);
             rot = Quaternion.RotateTowards(rot, toRot, speed * Time.deltaTime);
-
             stick.localRotation = rot;
         }
 
         public void SetDown(bool value)
         {
             m_isDown = value;
+            Debug.Log("SertDown Call: m_isDown = " + m_isDown);
         }
         public void OnCollisionStick(Collider collider)
         {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ namespace Golf
 
         private void Update()
         {
-            player.SetDown(Input.GetMouseButton(0));
+            player.SetDown(Input.GetMouseButton(0));          
         }
 
         private void Start()
